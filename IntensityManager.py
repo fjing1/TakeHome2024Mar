fe2fd7a9784664a96cfd54351e1cee047ecf5f36
Felix Jing <32583955+fjing1@users.noreply.github.com>
2024-03-15 09:22:56 -0400

    clean up the comment

--- IntensityManager.py
+++ IntensityManager.py
@@ -5,7 +5,6 @@ class IntensityManager:
 
     def get_turning_point_intensities(self):
         turning_point_intensities = []
-        #print("turning_point:",self.turning_point)
         prev_value = 0
         for k, value in self.intensities.items() :
             # If the key is in the turning_point set, add it to the turning_point_intensities list as a tuple
@@ -20,6 +19,8 @@ class IntensityManager:
         self.turning_point.add(end)
         self._update(start, end, amount, lambda x, y: x + y)
 
+    # Clarified with Tomoka Inoue, implemented with my assumptions
+    # Set behave same as add, update the starting point, but end point is not included.
     def set(self, start, end, amount):
         self.turning_point.add(start)
         self.turning_point.add(end)
@@ -30,14 +31,10 @@ class IntensityManager:
         end_intensity = self._get_intensity(end)
         for key in range(start, end):
             current_intensity = self._get_intensity(key)
-            print("current ",key,"intensity ", current_intensity)
-            # If the key already exists in the intensities dictionary, update its intensity
             self.intensities[key] = operation(current_intensity, amount)
         self.intensities[end] = operation(end_intensity, 0)
 
     def _get_intensity(self, request_point):
-        #print("turning point", self.turning_point)
-        #print("intensities", self.intensities)
         if request_point in self.intensities:
             return self.intensities[request_point]
         return 0
--- test_IntensityManager.py
+++ test_IntensityManager.py
@@ -4,11 +4,9 @@ def test_add_1st():
     manager = IntensityManager()
     # Test add method with 1st test case
     manager.add(10, 30, 1)
-    #print(manager.intensities, "expected to be [(10, 1), (30, 0)]")
     assert manager.get_turning_point_intensities() == [(10, 1), (30, 0)]
 
     manager.add(20, 40, 1)
-    #print(manager.intensities)
     assert manager.get_turning_point_intensities() == [(10, 1), (20, 2), (30, 1), (40, 0)]
 
     manager.add(10, 40, -2)
@@ -18,7 +16,6 @@ def test_add_1st():
 
 def test_add_2nd():
     im = IntensityManager()
-    # Test add method with 2nd test case
     im.add(10, 30, 1)
     print(im.get_turning_point_intensities())
     assert im.get_turning_point_intensities() == [(10, 1), (30, 0)]
@@ -40,3 +37,4 @@ print("start testing")
 test_add_1st()
 test_add_2nd()
 test_set_default()
+print("testing completed")
